971fff5f894df07be9de7750472c4d343ec0aae3
wh1t3st <alican15033@gmail.com>
2024-07-08 18:45:35 +0300

    deps added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            container: {
+                center: true,
+                screens: {
+                    "2xl": "800px",
+                },
+            },
+            backgroundImage: {
+                background: "url('/background.webp')",
+            },
+            fontFamily: {
+                jost: ["Jost", "sans-serif"],
+            }
+        },
+    },
+    plugins: [],
+};
