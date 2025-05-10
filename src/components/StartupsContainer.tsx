9d79321bc2f5f9a487509b5a4b255262e6c9333f
Sander de Groot <sander@degroot.dev>
2025-05-10 08:59:21 +0200

    Updated home text according to #12

--- src/components/StartupsContainer.tsx
+++ src/components/StartupsContainer.tsx
@@ -28,11 +28,16 @@ const StartupsContainer = ({ startups, startupFilters }: Props) => {
     return (
         <section>
             <img src={HeroLogo.src} alt="Logo" className="size-72 shrink-0 mx-auto" />
-            <h1 className="text-4xl font-bold tracking-tighter text-center my-4">Rail Startups</h1>
-            <p className="text-center text-pretty text-sm text-slate-800">
-                A curated list of rail startups. Not listed? <a className="text-teal-600" href="https://tally.so/r/wL0XN1" target="_blank">Register your startup here.</a>
+            <h1 className="text-4xl font-bold tracking-tighter text-center my-4">Welcome to the Rail Startups community</h1>
+            <p className="text-left text-pretty text-sm text-slate-800">
+                Across Europe, a new wave of entrepreneurs is reimagining the future of train travel. We believe <b>rail can be bigger, bolder, simpler—and yes, even sexier</b>.
+                From making train journeys more accessible to designing innovative services that attract more people to sustainable travel, we are full of good ideas. <b>Our challenge is to make our ideas and businesses viable within the current ecosystem of railway companies and major industry players.</b>
+            </p>
+            <p className="text-left text-pretty text-sm text-slate-800 pt-4">                                
+                That's why we're here—to <b>connect, support, and empower each other</b>. Together, we can take up more space, amplify our impact, and help rail take its rightful place at the heart of European mobility.
             </p>
             <div className="border-b border-teal-900/20 mx-auto w-2/3 my-6"></div>
+            <h2 className="text-3xl font-bold tracking-tighter text-center my-4">Our members</h2>
             <div className="grid items-center gap-4">
                 <StartupsFilters startupFilters={startupFilters} />
                 <StartupsSearch />
