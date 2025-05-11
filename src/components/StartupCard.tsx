4bcc078cf61a722646877b6c04f3340b319a7cd0
Sander de Groot <sander@degroot.dev>
2025-05-11 09:23:04 +0200

    added short description to the startups

--- src/components/StartupCard.tsx
+++ src/components/StartupCard.tsx
@@ -1,22 +1,25 @@
 import type { CollectionEntry } from "astro:content";
+import { MapPin } from "lucide-react";
 interface Props {
     startup: CollectionEntry<"startups">["data"];
 }
 const StartupCard = ({ startup }: Props) => {
     return (
-        <a
-            className="text-white animate-in fade-in duration-300 bg-gradient-to-r from-teal-600 to-teal-400 rounded-md p-4 overflow-hidden border border-teal-500 hover:border-teal-400 hover:bg-gradient-to-r hover:from-teal-500 hover:to-teal-300 transition-colors"
-            href={startup.website}
-            target="_blank"
-        >
-            <div className="flex gap-4">
-                <img src={startup.logo.src} alt={startup.companyName} width="100" height="100" />
-                <div>
-                    <h2 className="text-2xl font-bold">{startup.companyName}</h2>
-                    <p>{startup.country}</p>
+        <div className="text-white animate-in fade-in duration-300 bg-gradient-to-r from-teal-600 to-teal-400 rounded-md p-4 overflow-hidden border border-teal-500 hover:border-teal-400 hover:bg-gradient-to-r hover:from-teal-500 hover:to-teal-300 transition-colors">
+            <a
+                href={startup.website}
+                target="_blank"
+            >
+                <div className="flex gap-4">
+                    <img src={startup.logo.src} alt={startup.companyName} className="max-h-24 max-w-24" />
+                    <div>
+                        <h2 className="text-2xl font-bold">{startup.companyName}</h2>
+                        <p><MapPin className="inline"/> {startup.country}</p>
+                        <p><i>{startup.shortDescription}</i></p>
+                    </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
     );
 };
 
